b2913e4423887a857359c6f99b6fa0670148a4a2
clauney <clauney@gmail.com>
2018-04-25 21:39:02 -0700

    freaking tabs

--- tempsreadandupload.py
+++ tempsreadandupload.py
@@ -8,16 +8,16 @@ DHT_TYPE = Adafruit_DHT.DHT11
 DHT_PIN  = 4
 
 def getdata(DHT_TYPE, DHT_PIN):
-#	while True:
-	humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
-		#sometimes it sucks, particularly during high load. if at first you do not succeed...
-#		if humidity == None or temp == None:
-#			time.sleep(5)
-#			continue
-	return (temp, humidity)
+        #while True:
+        humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
+                #sometimes it sucks, particularly during high load. if at first you do not succeed...
+                #if humidity == None or temp == None:
+                        #time.sleep(5)
+                        #continue
+        return (temp, humidity)
 
 def convceltofar(tempcel):
-	return (tempcel*1.8)+32
+        return (tempcel*1.8)+32
 
 while True:
         print("getting data")
@@ -32,5 +32,4 @@ while True:
                 print("this would be me uploading to the azures")
                 print("temperature to upload:",convceltofar(tempreading))
                 print("humidity to upload:",humidityreading)
-                break
                 
